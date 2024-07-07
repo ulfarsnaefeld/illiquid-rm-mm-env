88ce42fba2bf9cd3197d497b61401a57dcc3a84d
ulfarsnaefeld <ulfarsnaefeld@gmail.com>
2024-07-07 21:57:26 +0000

    Implemented A2C and PPO

--- src/scripts/train_model.py
+++ src/scripts/train_model.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import gymnasium as gym
-from stable_baselines3 import DQN
+from stable_baselines3 import PPO, DQN, A2C
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.monitor import Monitor
 
@@ -23,8 +23,8 @@ params = {
 }
 
 # Function to train the model and save it
-def train_and_save_model(params, param_type, param_value):
-    log_dir = os.path.join('src/models', 'trained_models', 'DQN', param_type, str(param_value))
+def train_and_save_model(params, param_type, param_value, model_type='DQN'):
+    log_dir = os.path.join('src/models', 'trained_models', model_type, param_type, str(param_value))
     os.makedirs(log_dir, exist_ok=True)
 
     env = SeededMarketSimEnv(**params)
@@ -33,14 +33,18 @@ def train_and_save_model(params, param_type, param_value):
     # Check if the environment is valid
     check_env(env)
 
+    model = (
+        A2C("MlpPolicy", env, verbose=1) if model_type == 'A2C' else
+        PPO("MlpPolicy", env, verbose=1) if model_type == 'PPO' else
+        DQN("MlpPolicy", env, verbose=1))
+
     # Train the model
-    model = DQN("MlpPolicy", env, verbose=1)
     model.learn(total_timesteps=100_000)
 
     # Save the model
-    model.save(os.path.join(log_dir, f'market_making_DQN'))
+    model.save(os.path.join(log_dir, f'market_making_{model_type}'))
 
-    print(f"Model trained and saved for {param_type}={param_value}")
+    print(f"Model trained and saved for {param_type}={param_value} on {model_type}")
 
 # Train models for different parameters
 def train_all_models():
@@ -50,49 +54,63 @@ def train_all_models():
     base_params = {'s0': 100, 'T': 100, 'v': 1, 'q0': 0, 'c0': 100_000, 'sigma': 0.1, 'drift': 0.005, 'gamma': 0.95, 'itp': 0.2, 'ms': 0.03, 'tf': 1, 'itfp': 1, 'seed_sequence': seed_sequence}
     illiquid_params = {'s0': 100, 'T': 100, 'v': 1, 'q0': 0, 'c0': 100_000, 'sigma': 0.15, 'drift': 0.005, 'gamma': 0.95, 'itp': 0.4, 'ms': 0.03, 'tf': 0.5, 'itfp': 1 ,'seed_sequence': seed_sequence}
 
+    # Different types of RL models provided by stable_baselines3
+    model_types = ["DQN", "PPO", "A2C"]
+
     for sigma in params['sigma']:
         params_copy = base_params.copy()
         params_copy['sigma'] = sigma
-        train_and_save_model(params_copy, 'sigma', sigma)
+        for model_type in model_types:
+            train_and_save_model(params_copy, 'sigma', sigma, model_type)
 
     for drift in params['drift']:
         params_copy = base_params.copy()
         params_copy['drift'] = drift
-        train_and_save_model(params_copy, 'drift', drift)
+        for model_type in model_types:
+            train_and_save_model(params_copy, 'drift', drift, model_type)
+
 
     for gamma in params['gamma']:
         params_copy = base_params.copy()
         params_copy['gamma'] = gamma
-        train_and_save_model(params_copy, 'gamma', gamma)
+        for model_type in model_types:
+            train_and_save_model(params_copy, 'gamma', gamma, model_type)
 
     for itp in params['itp']:
         params_copy = base_params.copy()
         params_copy['itp'] = itp
-        train_and_save_model(params_copy, 'itp', itp)
+        for model_type in model_types:
+            train_and_save_model(params_copy, 'itp', itp, model_type)
 
     for ms in params['ms']:
         params_copy = base_params.copy()
         params_copy['ms'] = ms
-        train_and_save_model(params_copy, 'ms', ms)
+        for model_type in model_types:
+            train_and_save_model(params_copy, 'ms', ms, model_type)
 
     for tf in params['tf']:
         params_copy = base_params.copy()
         params_copy['tf'] = tf
-        train_and_save_model(params_copy, 'tf', tf)
+        for model_type in model_types:
+            train_and_save_model(params_copy, 'tf', tf, model_type)
 
     for inventory in params['inventory']:
         params_copy = base_params.copy()
         params_copy['q0'] = inventory
-        train_and_save_model(params_copy, 'inventory', inventory)
+        for model_type in model_types:
+            train_and_save_model(params_copy, 'inventory', inventory, model_type)
 
     for itfp in params['itfp']:
         params_copy = base_params.copy()
         params_copy['itfp'] = itfp
-        train_and_save_model(params_copy, 'itfp', itfp)
+        for model_type in model_types:
+            train_and_save_model(params_copy, 'itfp', itfp, model_type)
 
-    # train_and_save_model(base_params, 'liquid', '')
+    for model_type in model_types:
+        train_and_save_model(base_params, 'liquid', '', model_type)
 
-    # train_and_save_model(illiquid_params, 'illiquid', '')
+    for model_type in model_types:
+        train_and_save_model(illiquid_params, 'illiquid', '', model_type)
 
 if __name__ == "__main__":
     train_all_models()
